a315514fa52c6d03bcdfb739889253a9db8b236f
mayurbhambure <mayurbhambure11@gmail.com>
2022-10-23 21:32:19 -0400

    Code Enhancement is done as per issue

--- src/Addition.py
+++ src/Addition.py
@@ -1,9 +1,29 @@
 
+from unittest import result
+
+
 def addition(x,y):
+    '''
+    This Funaction take two number as a input and reteun sum of Number  
+    '''
     return x+y
 
+def userInput():
+    try:
+        a = int(input("Enter First Number"))
+        b = int(input("Enter Second Number"))
+        return a,b
+    except Exception:
+        print(" Please Enter Number Only")
+    
+
 if __name__ == '__main__':
-    a = int(input("Enter First Number"))
-    b= int(input("Enter Second Number"))
-    result = addition(a,b)
-    print("Addtion Of Two Numbers : - ",result)
+    try:
+        a,b = userInput()
+        result = addition(a,b)
+        if result is not None:
+            print("Addtion Of Two Numbers : - ",result)
+    except Exception as e:
+        print("Something went wrong with User inputs")
+    
+    
